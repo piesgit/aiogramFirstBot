730e6c2ca5bab0d781d6aab0e290f05868b4e9e8
piesgit <artyomgarmaza05@gmail.com>
2025-06-02 13:44:25 +0300

    4 lesson 6 minute 26 second

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,4 +4,6 @@ main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог')],
     [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
     
-])
+],
+                           resize_kyeboard=True,
+                           input_field_placeholder='Выберие пункт меню.')
